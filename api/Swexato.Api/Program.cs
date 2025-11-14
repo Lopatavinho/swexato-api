8289645454fad5b63a0d5712c3b9f59ff9d6e79e
Luiz Otávio Portes <lopa.portes@gmail.com>
2025-11-14 19:57:43 -0300

    chore: configurar Program.cs com DbContext e PessoaService

--- api/Swexato.Api/Program.cs
+++ api/Swexato.Api/Program.cs
@@ -1,41 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Swexato.Api.Data;
+using Swexato.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Connection string via environment variables (set by docker-compose)
+var conn = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? Environment.GetEnvironmentVariable("CONN_STRING")
+    ?? "Host=localhost;Port=5432;Database=swexato;Username=swexato;Password=swexato";
+
+builder.Services.AddDbContext<AppDbContext>(opts =>
+    opts.UseNpgsql(conn));
+
+builder.Services.AddScoped<PessoaService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    // NÃO execute migrations automaticamente em produção; para desenvolvimento aqui pode ser OK:
+    db.Database.EnsureCreated();
+}
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
